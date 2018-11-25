50a0895cc10ae2a1a2f839c1152cc50ec99069ba
risonakamo <hmykanae@gmail.com>
2018-11-25 15:00:20 -0500

    scuffed button functionalities

--- popup.js
+++ popup.js
@@ -1,4 +1,5 @@
 window.onload=main;
+var _sankakuImages=[];
 
 function main()
 {
@@ -9,8 +10,19 @@ function main()
         for (var x=0,l=tabs.length;x<l;x++)
         {
             chrome.tabs.executeScript(tabs[x].id,{file:"openoriginal.js",runAt:"document_end"},(res)=>{
-                console.log(res[0]);
+                _sankakuImages.push(res[0]);
             });
+        }
+
+        document.querySelector(".count").innerText=tabs.length;
+    });
+
+    document.querySelector(".sankaku-button").addEventListener("click",(e)=>{
+        e.preventDefault();
+
+        for (var x=0,l=_sankakuImages.length;x<l;x++)
+        {
+            chrome.tabs.create({url:_sankakuImages[x],active:false});
         }
     });
 }
