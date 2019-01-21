df7a110716436a67bb474c3b62e781c8d1d29d08
risonakamo <hmykanae@gmail.com>
2019-01-21 18:35:39 -0500

    original link detection improved

--- openoriginal.js
+++ openoriginal.js
@@ -1,6 +1,19 @@
-function main()
-{
-    return document.querySelector("#stats li:nth-of-type(3) a").href;
-}
+(()=>{
+    //origina link is usually 3rd li
+    var originalLinkLi=document.querySelector("#stats li:nth-of-type(3)");
 
-main();
+    if (originalLinkLi.innerText.slice(0,8)=="Original")
+    {
+        return originalLinkLi.firstElementChild.href;
+    }
+
+    //if its not
+    var allLis=document.querySelectorAll("#stats li");
+    for (var x=0,l=allLis.length;x<l;x++)
+    {
+        if (allLis[x].innerText.slice(0,8)=="Original")
+        {
+            return allLis[x].firstElementChild.href;
+        }
+    }
+})()
