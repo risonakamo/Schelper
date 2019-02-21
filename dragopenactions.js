7edf7ee0a6fb7d609ba843635275a605d2b4441e
risonakamo <hmykanae@gmail.com>
2019-02-21 16:15:53 -0500

    comments

--- dragopenactions.js
+++ dragopenactions.js
@@ -1,3 +1,5 @@
+//enables loop and pauses the first video element on the page
+//intended for file pages opening to a file which has a single video element.
 var player=document.querySelector("video");
 
 if (player)
